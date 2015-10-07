f0aea9e347e87095c9921861eee7ab3093e34d18
Sean Callan <seancallan@gmail.com>
2015-10-06 20:15:02 -0700

    Update templates to slim

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule PhoenixSlimExample.PageController do
   use PhoenixSlimExample.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "Informative text.")
+    |> put_flash(:error, "An error!")
+    |> render "index.html"
   end
 end
